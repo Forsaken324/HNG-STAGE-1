beeca13f7156fa9e2a8088ff164116106fb86bbf
David324325 <121452311+Forsaken324@users.noreply.github.com>
2025-02-03 00:34:38 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 import requests
 
@@ -62,4 +62,4 @@ def classify_number(request):
             "number" : "alphabet",
             "error" : True
         }
-        return Response(data, status=HTTP_404_NOT_FOUND)
+        return Response(data, status=HTTP_400_BAD_REQUEST)
